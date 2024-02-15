3d7bffdfe6d387d11c786286c64ec1c40dd8a671
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-15 18:46:32 +0600

    created a consumer in dotnet

--- dotnet/Receive/Program.cs
+++ dotnet/Receive/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Receive;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.UserName = "admin";
+        factory.Password = "admin";
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        var queue = "hello";
+
+        channel.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments:null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        };
+
+        channel.BasicConsume(
+            queue: queue,
+            autoAck: true,
+            consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
     }
 }
