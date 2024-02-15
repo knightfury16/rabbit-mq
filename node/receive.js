bd59a1b388c3fd8f68ebebfe99b0b80d60a14cf6
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-15 18:34:20 +0600

    to consume the queue property should be same, in all instance.

--- node/receive.js
+++ node/receive.js
@@ -7,15 +7,15 @@ const amqplib = require('amqplib');
 
     const ch2 = await conn.createChannel();
 
-    const queue = 'myqueue';
+    const queue = 'hello';
 
-    await ch2.assertQueue(queue)
+    await ch2.assertQueue(queue, { durable: false, exclusive: false, autoDelete: false, arguments: null })
 
     ch2.consume(queue, (msg) => {
-        if(msg !== null){
+        if (msg !== null) {
             console.log(msg.content.toString());
             ch2.ack(msg)
-        }else{
+        } else {
             console.log('consumer cancled by server')
         }
     })
