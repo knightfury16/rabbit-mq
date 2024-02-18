3c68fc0a51e4794955b4da7dfa9ea54523b4c913
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-18 19:08:25 +0600

    Implemented consumer to consume from specific routing key.

--- dotnet/routing/recieve/Program.cs
+++ dotnet/routing/recieve/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace recieve;
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace recieve;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var factory = new ConnectionFactory();
+
+        factory.HostName = "localhost";
+        factory.UserName = "admin";
+        factory.Password = "admin";
+
+        var cnn = factory.CreateConnection();
+
+        var chnl = cnn.CreateModel();
+
+        string exchange_name = "direct_logs";
+
+        chnl.ExchangeDeclare(exchange_name, ExchangeType.Direct);
+
+        var q1 = chnl.QueueDeclare();
+
+        chnl.QueueBind(q1.QueueName, exchange: exchange_name, routingKey: "Info");
+        //chnl.QueueBind(q1.QueueName, exchange: exchange_name, routingKey: "Info");
+
+        var consumer = new EventingBasicConsumer(chnl);
+
+        consumer.Received += msgConsumer;
+
+        chnl.BasicConsume(q1.QueueName, true, consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+
+    static void msgConsumer(object model, BasicDeliverEventArgs ea)
+    {
+        byte[] body = ea.Body.ToArray();
+        string msg = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine("Message received");
+        Console.WriteLine(msg);
     }
 }
