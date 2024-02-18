c50d16c65570506ca55c7daec6c3f9029347741f
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-18 16:53:23 +0600

    pub/sub implemented.

--- dotnet/pub-sub/Recieve/Program.cs
+++ dotnet/pub-sub/Recieve/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace Recieve;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Recieve;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var factory = new ConnectionFactory();
+
+        factory.HostName = "localhost";
+        factory.UserName = "admin";
+        factory.Password = "admin";
+
+        var cnn = factory.CreateConnection();
+
+        var chnl = cnn.CreateModel();
+
+        var q1 = chnl.QueueDeclare();
+        
+        chnl.QueueBind(q1.QueueName,"logs", string.Empty);
+
+        var consumer = new EventingBasicConsumer(chnl);
+
+        consumer.Received += msgConsumer;
+
+        chnl.BasicConsume(q1.QueueName, true, consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+
+    static void msgConsumer(object model, BasicDeliverEventArgs ea )
+    {
+        byte[] body = ea.Body.ToArray();
+        string msg = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine("Message received");
+        Console.WriteLine(msg);
     }
 }
--- dotnet/pub-sub/Send/Program.cs
+++ dotnet/pub-sub/Send/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Send;
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Send;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var factory = new ConnectionFactory(); 
+        factory.HostName = "localhost";
+        factory.UserName = "admin";
+        factory.Password = "admin";
+
+        IConnection connection = factory.CreateConnection();
+        IModel chnl = connection.CreateModel();
+
+        chnl.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+        var msg =Encoding.UTF8.GetBytes(GetMessage(args));
+
+        chnl.BasicPublish(exchange: "logs", routingKey: String.Empty, body: msg);
+
+        Console.WriteLine("Message sent to queue");
+        Console.ReadLine();
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        return (args.Length > 0) ? args[1] : "Empty String";
     }
 }
