ed88d0625d155b88d89d366e4861490492ccddc7
suhaib <suhaib.ahmmad@orbitax.com>
2024-02-19 12:36:37 +0600

    Created GetRabbitConnection class.

---
+++ dotnet/routing/send/GetRabbitConnection.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+
+namespace send
+{
+    internal class GetRabbitConnection
+    {
+        private static GetRabbitConnection _instance;
+        private readonly IConnection connection; 
+
+        private GetRabbitConnection()
+        {
+            var factory = new ConnectionFactory{ HostName = "localhost", UserName = "admin", Password = "admin" };
+            connection = factory.CreateConnection();
+        }
+
+        public static GetRabbitConnection Instance
+        {
+            get
+            {
+                _instance ??= new GetRabbitConnection();
+                return _instance;
+            }
+        }
+
+        public IConnection GetConnection()
+        {
+            return connection;
+        }
+
+    }
+}
